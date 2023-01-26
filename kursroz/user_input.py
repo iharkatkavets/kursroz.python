92a1333e55eea7c0dd9ecb8fd1c1205586630307
Ihar Katkavets <iharkatkavets@users.noreply.github.com>
2023-01-26 08:45:12 +0100

    main Add terminal formatting

--- kursroz/user_input.py
+++ kursroz/user_input.py
@@ -1,32 +1,33 @@
 from datetime import datetime
+from .print_utils import *
 
 def read_str(prompt):
     while True:
         try:
-            str = input(prompt)
+            str = input(f"{bcolors.BOLD}{prompt}{bcolors.ENDC}")
             return str
         except ValueError:
-            print("You've entered a wrong string. Try again...")
+            print(f"{bcolors.FAIL}You've entered a wrong string. Try again...{bcolors.ENDC}")
             pass
 
 def read_date(prompt, format):
     while True:
         try:
-            str = input(prompt)
+            str = input(f"{bcolors.BOLD}{prompt}{bcolors.ENDC}")
             date = datetime.strptime(str, format)
             return date
         except ValueError:
-            print("You've entered a wrong date. Try again...")
+            print(f"{bcolors.FAIL}You've entered a wrong date. Try again...{bcolors.ENDC}")
             pass
 
 def read_double(prompt): 
     while True:
         try:
-            str = input(prompt)
+            str = input(f"{bcolors.BOLD}{prompt}{bcolors.ENDC}")
             value = float(str)
             return str
         except ValueError:
-            print("You've entered a wrong value. Try again...")
+            print(f"{bcolors.FAIL}You've entered a wrong value. Try again...{bcolors.ENDC}")
             pass
 
 
